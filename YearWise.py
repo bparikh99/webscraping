8a1772ab90782da1c3f33c599a155ae4521e1ca0
Bhavya Parikh <59947941+bparikh99@users.noreply.github.com>
2022-10-08 11:18:51 +0530

    Add files via upload

--- YearWise.py
+++ YearWise.py
@@ -26,12 +26,20 @@ def make_call(year,month):
 def extract_details(post_link):
 
     images=[]
-    text_data,file,title="","",""
+    text_data=[]
+    filtered_tags=[]
+    file,title="",""
     try:
         res=requests.get(post_link)
         if res.status_code==200:
             soup=BeautifulSoup(res.text,"html.parser")
-            text_data=[i.get_text() for i in soup.find("div",class_="widget Blog").find_all("p",attrs={"style":"text-align: justify;"})]
+            # text_data=[i.get_text() for i in soup.find("div",class_="widget Blog").find_all("p",attrs={"style":"text-align: justify;"})]
+            text_lst=[""+"^"+i.get_text() if i.findChildren("table")  else i.get_text() for i in soup.find("div",class_="widget Blog").find_all("p",attrs={"style":"text-align: justify;"})]
+            for text in text_lst:
+                if text.startswith("^"):
+                    text_data.extend(text.split("^"))
+                else:
+                    text_data.append(text)
             title=soup.find("title").get_text()
             images=soup.find("div",class_="widget Blog").find_all("img")[:-1]
             file=post_link.split("/")[-1].split(".")[0]
@@ -48,30 +56,37 @@ def make_markdown(file,write_path,text_data,title,filtered_tags,count,year_path,
 
     try:     
         markdown_path=write_path+"\\"+year_path+"\\"+file+"\\"+file
-        mdFile = MdUtils(file_name=markdown_path+".md")
+        mdFile = MdUtils(file_name=markdown_path+"temp.md")
+        count=count+1
         format_data="---"+"\n"
-        val_title=' title: "{0}"'.format(title)
+        val_title='title: "{0}"'.format(title)
         count=count+1
         year=year_path.split("\\")[0]
         month=year_path.split("\\")[1]
         date_val=f"{count}-{month}-{year}"
-        date=' date: "{0}"'.format(date_val)
-        oldUrl=' oldUrl: "{0}"'.format(post_link)
+        date="date: {0}".format(date_val)
+        oldUrl="oldUrl: {0}".format(post_link)
 
         mdFile.new_line(format_data+val_title)
         mdFile.new_line(date)
         mdFile.new_line(oldUrl)
-        mdFile.new_line(" tag:")
+        mdFile.new_line("tag:")
         mdFile.new_list(items=filtered_tags,marked_with="  -")      
         mdFile.write(format_data)
-
         for text in text_data:
             if ".jpg" in text:
                 mdFile.new_line(mdFile.new_inline_image(text="image", path=text))
             else:
                 mdFile.new_paragraph(text)
-            mdFile.write('  \n')
+            mdFile.write('\n')
         mdFile.create_md_file()
+        md_data=mdFile.read_md_file(markdown_path+"temp.md")
+        md_data=md_data.lstrip("\n").strip().lstrip("\n")
+        with open(markdown_path+".md" ,"w") as final_data:
+            final_data.write(md_data)
+
+        #os.rename(markdown_path+"temp.md", markdown_path+".md")
+        os.remove(markdown_path+"temp.md")
     except Exception as e:
         print("Error while writing data for markdown file "+str(e))
 
@@ -97,7 +112,7 @@ def extract_images(images,write_path,file,empty_text,year_path):
         if text=="":
             text_index=text_data.index(text)
             try:
-                local_img_path="images"+"\\"+img_files[empty_text-1]
+                local_img_path="images"+"/"+img_files[empty_text-1]
                 text_data[text_index]=local_img_path
                 empty_text=empty_text+1
             except:
@@ -118,7 +133,10 @@ if __name__ == "__main__":
         print("path exists")
     os.chdir(write_path)
     year=int(input("Enter Year between 2012 -2021 : "))
-    month=int(input("Enter Month : "))
+    month=input("Enter Month : ")
+    if len(month)==1:
+        month="0"+month
+        
     
     print(f"Downloading Posts for year: {year}, month: {month}")
 
